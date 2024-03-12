b9a19837b8aa48575c628db63492c3432e704730
chiyari <vikas.halder_bca20@gla.ac.in>
2024-03-12 15:27:36 +0530

    Server files updated by

--- Psync.bat
+++ Psync.bat
@@ -1,16 +1,8 @@
 @echo off
 
-- Your server shutdown logic here
 echo Server shutting down...
-- Add all modified files to the staging area
 git add .
-
-- Commit the changes with a custom message by the user who committed
 for /f "usebackq tokens=2" %%G in (`git config --get user.name`) do set git_user=%%G
 git commit -m "Server files updated by %git_user%"
-
-- Push the changes to the repository
 git push
-
-- Optional: Display a success message with green color
 echo Server files updated successfully
