131d595e6a205ee73c66dfde4e2edf5012a8d8b7
chiyari <vikas.halder_bca20@gla.ac.in>
2024-03-12 17:12:44 +0530

    Updated psync

--- Psync.bat
+++ Psync.bat
@@ -2,7 +2,6 @@
 
 echo Server shutting down...
 git add .
-for /f "usebackq tokens=2" %%G in (`git config --get user.name`) do set git_user=%%G
-git commit -m "Server files updated by %git_user%"
+git commit -m "Server files updated by %git config --get user.name%"
 git push
 echo Server files updated successfully
