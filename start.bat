e7b2a396ad5915365071a5e7ce13ee54b45bdfd4
chiyari <vikas.halder_bca20@gla.ac.in>
2024-03-12 15:21:14 +0530

    Server files updated by

---
+++ start.bat
@@ -0,0 +1,14 @@
+REM pulling the code from the origin 
+git pull --ff-only 
+
+
+java -Xmx8192M -Xms8192M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar 
+
+echo Server stopped.
+REM run the Psync.bat file to push the code to the github repository
+call Psync.bat
+REM show different coloured messages to the user
+echo [32mCode pushed to the repository !! also  you are gay [0m
+REM dont close the window after the process is done
+pause
+```
