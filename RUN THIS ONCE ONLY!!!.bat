4c4f03d408a64e26d0e8f2a0f558e49e1f77d5e7
= <=>
2024-03-12 16:55:06 +0530

    Server files updated by

--- RUN THIS ONCE ONLY!!!.bat
+++
@@ -1,7 +0,0 @@
-echo [31mThis is a one time process. Please make sure you have the latest code before running this script.[0m 
-git init
-git remote add origin https://github.com/VoidVikas/Payalcraft.git
-git pull --ff-only 
-git checkout main
-echo [32mProcess is done[0m 
-pause
