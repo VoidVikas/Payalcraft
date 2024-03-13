86454a202ed42af2081111140adc5d540cf16506
chiyari <vikas.halder_bca20@gla.ac.in>
2024-03-13 16:35:31 +0530

    Update commit message to include commit time

--- Psync.bat
+++ Psync.bat
@@ -2,7 +2,8 @@
 
 echo Server shutting down...
 git add .
-git commit -m "Server files updated by %git config --get user.name%"
+set "commit_time=%date% %time%"
+git commit -m "Server files updated at %commit_time%"
 git push
 echo Server files updated successfully
-git log -1 --pretty=format:"Last commit by: %an"
+git log -1 --pretty=format:"Last commit by: %an%nLast commit time (IST): %ai"
