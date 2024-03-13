9015d1314b5a90cde2f1ae6519fa5c51a8b794f0
chiyari <vikas.halder_bca20@gla.ac.in>
2024-03-13 16:18:53 +0530

    Server files updated by

--- Psync.bat
+++ Psync.bat
@@ -2,6 +2,9 @@
 
 echo Server shutting down...
 git add .
-git commit -m "Server files updated by %git config --get user.name%"
+git commit -m "Server files updated by %USERNAME%"
 git push
 echo Server files updated successfully
+
+for /f "delims=" %%a in ('git log -1 --format^=%an') do set last_commit_author=%%a
+echo Last commit author: %last_commit_author%
