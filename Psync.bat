7277379e37012630573dfce711abfa9320f94326
chiyari <vikas.halder_bca20@gla.ac.in>
2024-03-13 22:51:07 +0530

    Update Psync.bat script

--- Psync.bat
+++ Psync.bat
@@ -6,4 +6,4 @@ set "commit_time=%date% %time%"
 git commit -m "Server files updated at %commit_time%"
 git push
 echo Server files updated successfully
-git log -1 --pretty=format:"Last commit by: %an%nLast commit time (IST): %ai"
+
